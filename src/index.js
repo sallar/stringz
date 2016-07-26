d98371f06e6ab28262310f5018c76819b579009f
Salar Kaboli <sallar.kaboli@gmail.com>
2016-07-26 22:00:11 +0300

    Change export style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import substring from "unicode-substring";
 
-export default function limitStringLength(str, limit = 16, padString = "#", padPosition = "right") {
+export default function(str, limit = 16, padString = "#", padPosition = "right") {
     if (typeof str !== "string" || typeof limit !== "number") {
         throw new Error("Invalid arguments specified");
     }
