dc96d85afcc47852caeefe143b7de5edac25619c
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-05-25 22:39:32 +0300

    Add Typescript definition files

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare namespace stringz {
+  export function length(str: string): number;
+  export function substring(str: string, begin?: number, end?: number): string;
+  export function substr(str: string, begin?: number, length?: number): string;
+  export function limit(str: string, limit?: number, padString?: string, padPosition?: 'right'|'left');
+}
+
+export = stringz;
