96082661071519260f8e2f069fb4aadd4299b2b8
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-08-16 20:58:59 +0300

    Add benchmarks

---
+++ benchmark/run.js
@@ -0,0 +1,36 @@
+const Benchmark = require('benchmark')
+const EmojiAware = require('emoji-aware')
+const Stringz = require('stringz')
+const Spliddit = require('spliddit')
+const Bytes = require('utf8-length')
+const _ = require('lodash')
+
+const suite = new Benchmark.Suite()
+const str = 'Iñtërnâtiônàlizætiøn☃👍🏽💩😊💪🏼😻'
+
+// add tests
+suite
+  .add('Stringz .length (accruate)', function () {
+    return Stringz.length(str)
+  })
+  .add('Lodash .toArray (accruate)', function () {
+    return _.toArray(str).length
+  })
+  .add('Emoji Aware .split (inaccurate)', function () {
+    return EmojiAware.split(str).length
+  })
+  .add('Spliddit .length (inaccurate)', function () {
+    return Spliddit(str).length
+  })
+  .add('UTF8 Length (inaccurate)', function () {
+    return Bytes(str)
+  })
+  // add listeners
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  // run async
+  .run({ 'async': true })
