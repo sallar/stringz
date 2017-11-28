7eae9e0bb096cec5c61cd5a8f79355ab578b2e90
wangwenkai <wangwenkai2014@xiaochuankeji.cn>
2017-11-28 20:38:42 +0800

    add type definition

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ declare namespace stringz {
   export function substring(str: string, begin?: number, end?: number): string;
   export function substr(str: string, begin?: number, length?: number): string;
   export function limit(str: string, limit?: number, padString?: string, padPosition?: 'right'|'left') : string;
+  export function indexOf(str: string, searchStr: string, position?: number): number;
 }
 
 export = stringz;
